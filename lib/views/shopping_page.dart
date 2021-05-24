b9895719cfbe2e0e93637a5fe3aab85e0cf587e9
Trung Hieu <hieutt@d-soft.com.vn>
2021-05-24 16:19:08 +0700

    Add getX

---
+++ lib/views/shopping_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controllers/shopping_controller.dart';
+
+class ShoppingPage extends StatelessWidget {
+  final shoppingController = Get.put(ShoppingController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: GetX<ShoppingController>(
+                builder: (controller) {
+                  return ListView.builder(
+                    itemCount: controller.products.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        margin: const EdgeInsets.all(12),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        '${controller.products[index].productName}',
+                                        style: TextStyle(fontSize: 24),
+                                      ),
+                                      Text(
+                                          '${controller.products[index].productDescription}'),
+                                    ],
+                                  ),
+                                  Text('\$${controller.products[index].price}',
+                                      style: TextStyle(fontSize: 24)),
+                                ],
+                              ),
+                              RaisedButton(
+                                onPressed: () {},
+                                color: Colors.blue,
+                                textColor: Colors.white,
+                                child: Text('Add to Cart'),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
